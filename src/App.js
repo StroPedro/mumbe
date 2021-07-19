9f376bd87a4433eab1ca39ef067121e1713835fa
pedro <pedronndong777@gmail.com>
2021-07-19 14:44:13 +0100

    personalizando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> pedro ndong ondo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>pedro ndong ondo</h1>
     </div>
   );
 }
