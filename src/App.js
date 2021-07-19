9db15fef22b67f73f618129283bb45676e078606
pedro <pedronndong777@gmail.com>
2021-07-19 13:47:28 +0100

    html pedro

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> pedro ndong ondo
         </p>
         <a
           className="App-link"
